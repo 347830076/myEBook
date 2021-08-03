b7e11bf8f56ebc3555bb5ccc3d049fcc7da6add0
阿离王 <guoyu@comliq.com>
2021-08-03 23:17:09 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/347830076/myEBook.git main:gh-pages
+git push -f https://github.com/347830076/myEBook.git master:gh-pages
 
 cd -
